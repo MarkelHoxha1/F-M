ce1536048b94b76e951897c090d50f6034e98173
Markel Hoxha <30394539+MarkelHoxha1@users.noreply.github.com>
2017-10-09 01:28:52 +0200

    Login and Register

---
+++ Project/index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>Financial Management</title>
+<link rel="stylesheet" type="text/css" href="../CssProject/mystyle.css"> 
+<link rel="stylesheet" href="../Css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> 
+<script language="JavaScript" type="text/javascript" src="../Javascript/modification.js"></script>
+<script  language="JavaScript" type="text/javascript" src= "../Javascript/jquery.min.js"></script>
+</head>
+<body>
+    
+<div class="container">
+
+<div class="row" style="margin-top:20px">
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+		<form role="form">
+			<fieldset>
+				<h2>Financial Management</h2>
+				<hr class="colorgraph">
+				<div class="form-group">
+                    <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Username">
+				</div>
+				<div class="form-group">
+                    <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password">
+				</div>
+				<span class="button-checkbox">
+					<button type="button" class="btn" data-color="info">Remember Me</button>
+                    <input type="checkbox" name="remember_me" id="remember_me" checked="checked" class="hidden">
+					<a href="" class="btn btn-link pull-right">Forgot Password?</a>
+				</span>
+				<hr class="colorgraph">
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+                        <input type="submit" id='login' class="btn btn-lg btn-success btn-block" value="Sign In">
+					</div>
+	
+				<div class="col-xs-6 col-sm-6 col-md-6">
+		
+				<a href="Register.php"  class="btn btn-lg btn-primary btn-block">Register</a>
+
+					</div>
+				</div>
+			
+</fieldset>
+		</form>
+	</div>
+</div>
+
+</div>
+</body>
+</html>
